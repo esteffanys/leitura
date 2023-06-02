91d243f0192c7ca45a461773cad2bd28a5afaee8
esteffanys <97310047+esteffanys@users.noreply.github.com>
2023-06-02 22:08:45 +0000

    funcionando

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,33 +1,44 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { LeitorService } from '../model/leitor.service';
-import { RangeCustomEvent } from '@ionic/angular';
-import { RangeValue } from '@ionic/core';
+// import { RangeCustomEvent } from '@ionic/angular';
+// import { RangeValue } from '@ionic/core';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 formTxt:FormGroup;
-taxa!: RangeValue
-tom!:RangeValue
+
+
+pinFormatter(value: number) {
+  return `${value}%`;
+}
   constructor(private ls:LeitorService,private fb:FormBuilder) {
 this.formTxt = fb.group({
-  texto:['',Validators.required]
+  texto:['',Validators.required],
+  taxa:[0],
+  tom:[1]
+
 })
 }
-
+  
 ler(){
-  this.ls.ler(this.formTxt.get('texto')?.value);
+  this.ls.ler(this.formTxt.get('texto')?.value,
+  this.formTxt.get('taxa')?.value,
+  this.formTxt.get('tom')?.value)
+    console.log(this.formTxt.get('taxa')?.value,
+  this.formTxt.get('tom')?.value)
 }
-vertaxa(event: Event){
-  this.taxa = (event as RangeCustomEvent).detail.value;
-}
+// vertaxa(event: Event){
+//   this.taxa = (event as RangeCustomEvent).detail.value;
+// }
 
-vertom(event: Event){
-  this.tom = (event as RangeCustomEvent).detail.value;
-}
+// vertom(event: Event){
+//   this.tom = (event as RangeCustomEvent).detail.value;
+// }
 
 parar(){
   this.ls.parar();
